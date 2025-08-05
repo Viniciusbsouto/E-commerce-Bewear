dd5e0362a32c5f5354a4d0d6b6d33cdb2b67d666
Vinicius <viniciusbsouto@gmail.com>
2025-08-04 21:50:44 -0300

    shadcn adicionado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const home = () => {
   return (
-    <div className="m-8">
-      <h1 className="text-3xl font-bold underline">Home</h1>
+    <div className="m-8 flex justify-center items-center">
+      <h1 className="text-3xl bg-red-500 inline p-2 rounded-md font-bold ">
+        Home
+      </h1>
     </div>
   );
 };
